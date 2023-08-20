4c7b2a13984fd1c1991e25644cc967a39019bf8b
Apllify <ralilahlou50@gmail.com>
2023-08-20 17:36:02 +0100

    monogame template

---
+++ GameConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UntitledMagusProject
+{
+	public static class GameConstants
+	{
+		//virtual resolution
+		//1920/384 = 1080/216 = 5	
+		//384/32 = 12
+		//216/32 = 6.75
+		public const int screenWidth = 384 ;
+		public const int screenHeight = 216;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new UntitledMagusProject.MageGame();
+game.Run();
