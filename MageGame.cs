8ac2ed61bd4887f46af37afa9bb37144d1708f03
illuminati20043 <illuminati20043@gmail.com>
2023-08-21 20:25:02 +0200

    alj

--- MageGame.cs
+++ MageGame.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 
 namespace UntitledMagusProject
 {
@@ -12,8 +13,10 @@ namespace UntitledMagusProject
 		private RenderTarget2D _renderTarget; //used for virtual resolution
 
 		private Texture2D ballSprite;
+        private TiledMap _tiledMap;
+        private TiledMapRenderer _tiledMapRenderer;
 
-		public MageGame()
+        public MageGame()
 		{
 			_graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
@@ -32,8 +35,12 @@ namespace UntitledMagusProject
 		{
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			// TODO: use this.Content to load your game content here
-			ballSprite = Content.Load<Texture2D>("Placeholders/ball");
+           
+
+
+
+            // TODO: use this.Content to load your game content here
+            ballSprite = Content.Load<Texture2D>("Placeholders/ball");
 		}
 
 		protected override void Update(GameTime gameTime)
@@ -41,9 +48,9 @@ namespace UntitledMagusProject
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
 
-			// TODO: Add your update logic here
+            // TODO: Add your update logic here
 
-			base.Update(gameTime);
+            base.Update(gameTime);
 		}
 
 		protected override void Draw(GameTime gameTime)
@@ -53,15 +60,13 @@ namespace UntitledMagusProject
 
 			GraphicsDevice.SetRenderTarget(_renderTarget);
 
-			_spriteBatch.Begin();
-			_spriteBatch.Draw(ballSprite, new Vector2(0, 0), Color.White);
-			_spriteBatch.End();
-
-			
+            GraphicsDevice.Clear(Color.Black);
 
 
 
-			GraphicsDevice.SetRenderTarget(null); // Backbuffer
+
+
+            GraphicsDevice.SetRenderTarget(null); // Backbuffer
 
 			_spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend, SamplerState.PointClamp);
 			_spriteBatch.Draw(_renderTarget, new Rectangle(0, 0, Window.ClientBounds.Width, Window.ClientBounds.Height), Color.White);
